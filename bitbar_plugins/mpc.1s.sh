42ea8c1166c8fd999c5c73fb39d8c45e3c17717a
Ken Yap Wei Chun <wei.chun@pickupp.io>
2019-02-14 12:48:11 +0800

    skhdrc, vimrc, zshrc update along with bitbar update, vimrc revamp

--- bitbar_plugins/mpc.1s.sh
+++ bitbar_plugins/mpc.1s.sh
@@ -9,7 +9,17 @@ if [[ "$1" = "pause" ]]; then
   mpc pause
 fi
 
+if [[ "$1" = "next" ]]; then
+  mpc next
+fi
+
+if [[ "$1" = "prev" ]]; then
+  mpc prev
+fi
+
 echo "$(mpc status | awk 'NR==1{print substr($0, index($0,$100))}') | length=50"
 echo $(mpc status | awk 'NR==2 { split($3, a, "/"); print a[1] }')
 echo "Pause | bash='$0' param1=pause terminal=false"
 echo "Play | bash='$0' param1=play terminal=false"
+echo "Next | bash='$0' param1=next terminal=false"
+echo "Previous | bash='$0' param1=prev terminal=false"
