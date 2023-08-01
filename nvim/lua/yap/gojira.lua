91d065fd3d83313cb5c8148be7788f826318dd41
Ken Yap Wei Chun <wei.chun@pickupp.io>
2023-08-01 23:40:50 +0800

    feat: add gojira initial commit

---
+++ nvim/lua/yap/gojira.lua
@@ -0,0 +1,9 @@
+-- local gojira = require('gojira')
+--
+-- gojira.setup({
+--   host = 'pickupp',
+--   username = 'wei.chun@pickupp.io',
+--   accessToken = 'ujSHZxvyF5OPbnonbvTF4F38',
+--
+--   projectFilter = '(Hercules|Chemical X)',
+-- })
