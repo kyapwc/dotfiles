b1b60f678b68f140ae018182b02122d5e3ddff2c
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-02-02 18:37:52 +0800

    feat: update sketchybar with media_ctrl

--- sketchybar/plugins/media_ctrl.sh
+++ sketchybar/plugins/media_ctrl.sh
@@ -26,7 +26,23 @@ update ()
     APP="$(base64 -d <<< $(/opt/homebrew/bin/nowplaying-cli get clientPropertiesData) | awk -F ':' '{print $2}')"
     MEDIA="$APP: $TRACK - $ARTIST"
     PLAYBACK_RATE=$(/opt/homebrew/bin/nowplaying-cli get playbackRate)
+
+    media_duration=$(/opt/homebrew/bin/nowplaying-cli get duration)
+    elapsed_time=$(/opt/homebrew/bin/nowplaying-cli get elapsedTime)
+
+    elapsed_minutes=$(awk -v dur="$elapsed_time" 'BEGIN {print int(dur/60)}')
+    elapsed_seconds=$(awk -v dur="$elapsed_time" 'BEGIN {printf "%02.0f", dur % 60}')
+
+    duration_minutes=$(awk -v dur="$media_duration" 'BEGIN {print int(dur/60)}')
+    duration_seconds=$(awk -v dur="$media_duration" 'BEGIN {printf "%02.0f", dur % 60}')
+
+    ELAPSED="${elapsed_minutes}:${elapsed_seconds}"
+    DURATION="${duration_minutes}:${duration_seconds}"
+    PROGRESS=$(printf "%.2f" $(echo "($elapsed_time / $media_duration) * 100" | bc -l))
   fi
+
+  # echo "ELAPSED: ${ELAPSED}" >> test_progress.txt
+  # echo "DURATION: ${DURATION}" >> test_progress.txt
 
   args=()
   if [ $PLAYING -eq 0 ]; then
@@ -34,11 +50,15 @@ update ()
     if [ "$ARTIST" == "" ]; then
       args+=(--set media_ctrl.title label="$TRACK"
              --set media_ctrl.album label="Podcast"
-             --set media_ctrl.artist label="$ALBUM"  )
+             --set media_ctrl.artist label="$ALBUM"
+             --set media_ctrl.playback_slider slider.percentage=$PROGRESS
+             --set media_ctrl.playback label="${ELAPSED} / ${DURATION}" )
     else
       args+=(--set media_ctrl.title label="$TRACK"
              --set media_ctrl.album label="$ALBUM"
-             --set media_ctrl.artist label="$ARTIST")
+             --set media_ctrl.artist label="$ARTIST"
+             --set media_ctrl.playback_slider slider.percentage="$PROGRESS"
+             --set media_ctrl.playback label="${ELAPSED} / ${DURATION}" )
     fi
     args+=(--set media_ctrl.play icon=
            --set media_ctrl.cover background.image="/tmp/cover.jpg"
@@ -47,6 +67,8 @@ update ()
     if [ "$PLAYBACK_RATE" -eq 0 ]; then
       args+=(--set media_ctrl.play icon=󰐊                         )
     fi
+  else
+    args+=(--set media_ctrl.anchor label="-" drawing=on)
   fi
   sketchybar -m "${args[@]}"
 }
