66d88176c2e31722ac940bb4a6eb2593ea54ecc6
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-05-01 01:58:26 +0800

    feat: try disabling notify

--- nvim/.config/nvim/lua/yap/noice.lua
+++ nvim/.config/nvim/lua/yap/noice.lua
@@ -4,6 +4,9 @@ noice.setup({
   messages = {
     enabled = false,
   },
+  notify = {
+    enabled = false,
+  },
   lsp = {
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
