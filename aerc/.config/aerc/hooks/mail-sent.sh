0ef1d1cf518e3000193bc5a19cd753c4fdd43d03
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-06 17:31:09 +0800

    feat(aerc/khard): integrate khard to aerc

--- aerc/.config/aerc/hooks/mail-sent.sh
+++ aerc/.config/aerc/hooks/mail-sent.sh
@@ -7,4 +7,20 @@
 # AERC_TO
 # AERC_CC
 
-osascript -e "display notification \"[$AERC_ACCOUNT] SENT EMAIL WITH SUBJECT: $AERC_SUBJECT\" with title \"[$AERC_ACCOUNT] SENT EMAIL TO <$AERC_TO>\""
+USER_MAIL=$(echo $AERC_TO | sed 's/[<>]//g')
+USER_EXISTS=$(khard email "$USER_MAIL")
+
+echo "$USER_EXISTS" >> ~/.contacts/gmail/temp.txt
+echo "$USER_MAIL" >> ~/.contacts/gmail/temp.txt
+
+if [ "$USER_EXISTS" = "Found no email addresses" ]; then
+  echo "
+Formatted name : $USER_MAIL
+First name : $USER_MAIL
+Email :
+  work : $USER_MAIL
+" | khard new
+  echo "Added new user" >> ~/.contacts/gmail/temp.txt
+fi
+
+osascript -e "display notification \"[$AERC_ACCOUNT] SENT EMAIL WITH SUBJECT: $AERC_SUBJECT\" with title \"[$AERC_ACCOUNT] SENT EMAIL TO $AERC_TO\""
