1c6aa84b0e37c3e74ca234e7da6f19b2a8552f33
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-10 10:20:11 +0800

    feat(tmux): remove tmux entirely

--- dotfiles_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dotfiles=(".vimrc" ".zshrc" ".gitconfig" ".bash_profile" ".gitignore_global" "oh-my-zsh")
-dir=$HOME/dotfiles
-
-for dotfile in "${dotfiles[@]}";do
-  ln -sf "${HOME}/${dotfile}" "${dir}"
-done
