b11eab8a0ed322237c0f19fd84ffc3815749fa2b
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-11 11:45:45 +0800

    feat(nvim): move all autocmds to its own module
    - cleaner init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -41,58 +41,12 @@ require('impatient')
 -- =======================
 local vim = vim
 
--- =======================
--- Misc vim autocmds
--- =======================
-vim.cmd[[
-  tnoremap <Esc> <C-\><C-n>
-  autocmd Filetype python setlocal ts=2 sw=2 expandtab
-  nnoremap <expr> j v:count ? 'j' : 'gj'
-  nnoremap <expr> k v:count ? 'k' : 'gk'
-  autocmd FileType python map <buffer> <F9> :exec '!python3' shellescape(@%, 1)
-  let g:python3_host_prog = '/usr/local/bin/python3'
-  nnoremap <silent> <C-6> <C-^>
-  set noswapfile
-]]
-
--- =======================
--- Setting sh filetype to bash
--- =======================
-vim.cmd([[
-  augroup _astro
-  autocmd!
-  autocmd BufRead,BufEnter *.sh set filetype=bash
-  augroup end
-]])
-
--- =======================
--- Personal text objects
--- =======================
-vim.cmd[[
-  onoremap iq i'
-  onoremap iQ i"
-  onoremap aq a'
-  onoremap aQ a"
-  onoremap sq s'
-  onoremap sQ s"
-  onoremap , t,
-  vnoremap iq i'
-  vnoremap iQ i""
-]]
-
--- =======================
--- Splitjoin config
--- =======================
-vim.cmd[[
-  let g:splitjoin_trailing_comma = 1
-]]
-
 -- ======================
 -- Tokyonight config
 -- ======================
 require('yap/tokyonight')
 vim.cmd[[
-colorscheme tokyonight
+  colorscheme tokyonight
 ]]
 vim.g.colors_name = 'tokyonight-moon'
 vim.g.tokyonight_style = 'night' -- storm / night / day / moon
@@ -314,61 +268,6 @@ vim.cmd[[
   endif
 ]]
 
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
-})
-
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.prisma",
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-  group = format_sync_grp,
-})
-
-vim.cmd [[
-  inoremap <silent><expr> <CR> pumvisible() ? "\<C-y><CR>" : "\<CR>"
-  set scrolloff=5
-]]
-
-vim.api.nvim_create_autocmd("UIEnter", {
-  callback = function ()
-    local groups = {
-      "Normal",
-      "NormalNC",
-      "NormalFloat",
-      "Float",
-      "FloatBorder",
-      "SignColumn",
-      "GitSignsAdd",
-      "GitSignsChange",
-      "GitSignsDelete",
-      -- "Pmenu",
-      -- "PmenuSel",
-      "WinSeparator",
-      "TelescopeNormal",
-      "TelescopeBorder",
-      "TelescopeSelection",
-      "TelescopePreviewNormal",
-      "WhichKeyFloat",
-    }
-
-    for _, group in ipairs(groups) do
-      -- vim.cmd("hi " .. group .. " ctermbg=None guibg=None")
-      vim.cmd("hi " .. group .. " guibg=None")
-    end
-
-    return true
-  end,
-  desc = "Transparent backgrounds",
-})
-
 local function switch_case()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   local word = vim.fn.expand('<cword>')
@@ -389,6 +288,7 @@ vim.keymap.set('n', '<Space>ss', switch_case, {noremap = true, silent = true})
 -- vim.opt.termguicolors = true
 
 -- require('yap/noice')
+require('yap/autocmds')
 require('yap/telescope')
 require('yap/retrail')
 require('yap/treesitter')
@@ -406,7 +306,6 @@ require('yap/minimap')
 require('yap/alpha')
 require('yap/formatter')
 require('yap/luasnip')
-require('yap/autocmds')
 -- require('yap/dressing')
 require('yap/oil')
 require('yap/smart-splits')
@@ -416,6 +315,7 @@ require('yap/navic')
 require('yap/quicknote')
 require('yap/global-note')
 require('yap/whichkey')
+require('yap/no-neck-pain')
 -- require('yap/rest')
 -- require('yap/inlay-hints')
 -- require('fidget').setup({
--- nvim/.config/nvim/lua/yap/autocmds.lua
+++ nvim/.config/nvim/lua/yap/autocmds.lua
@@ -1,41 +1,119 @@
-local notify = require('notify')
+-- =======================
+-- Misc vim autocmds
+-- =======================
+vim.cmd[[
+  tnoremap <Esc> <C-\><C-n>
+  autocmd Filetype python setlocal ts=2 sw=2 expandtab
+  nnoremap <expr> j v:count ? 'j' : 'gj'
+  nnoremap <expr> k v:count ? 'k' : 'gk'
+  autocmd FileType python map <buffer> <F9> :exec '!python3' shellescape(@%, 1)
+  let g:python3_host_prog = '/usr/local/bin/python3'
+  nnoremap <silent> <C-6> <C-^>
+  set noswapfile
+]]
 
-local api = vim.api
+-- =======================
+-- Setting sh filetype to bash
+-- =======================
+vim.cmd([[
+  augroup _astro
+  autocmd!
+  autocmd BufRead,BufEnter *.sh set filetype=bash
+  augroup end
+]])
 
-local nestGroup = api.nvim_create_augroup('NestGroup', { clear = true })
--- api.nvim_create_autocmd({ 'BufEnter' }, {
---   pattern = '*.service.ts',
---   callback = function()
---     local bufNo = api.nvim_get_current_buf()
---
---     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
---     if #lines > 1 then
---       return
---     end
---
---     if vim.api.nvim_get_mode().mode == "t" then
---       local escape = vim.api.nvim_replace_termcodes("<Esc>i", true, false, true)
---       vim.api.nvim_feedkeys(escape, "n", false)
---     end
---
---     local windowNo = api.nvim_get_current_win()
---     vim.ui.input({
---       prompt = 'Class name',
---     }, function(input)
---       if input ~= nil then
---         api.nvim_buf_set_lines(bufNo, 0, 0, false, {
---           [[import { Injectable } from '@nestjs/common';]],
---           [[]],
---           [[@Injectable()]],
---           [[export class ]] .. input .. [[ {}]],
---         })
---
---         api.nvim_win_set_cursor(windowNo, { 4, 13 })
---       end
---     end)
---     -- api.nvim_win_set_cursor(windowNo, { 4, 13 })
---     -- api.nvim_input('ve')
---   end,
---   group = nestGroup,
---   once = true,
--- })
+-- =======================
+-- Personal text objects
+-- =======================
+vim.cmd[[
+  onoremap iq i'
+  onoremap iQ i"
+  onoremap aq a'
+  onoremap aQ a"
+  onoremap sq s'
+  onoremap sQ s"
+  onoremap , t,
+  vnoremap iq i'
+  vnoremap iQ i""
+]]
+
+-- =======================
+-- Splitjoin config
+-- =======================
+vim.cmd[[
+  let g:splitjoin_trailing_comma = 1
+]]
+
+-- ======================
+-- Tokyonight config
+-- ======================
+vim.cmd[[
+  colorscheme tokyonight
+]]
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
+local prismaSyncGrp = vim.api.nvim_create_augroup("PrismaSyncGrp", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.prisma",
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+  group = prismaSyncGrp,
+})
+
+vim.cmd [[
+  inoremap <silent><expr> <CR> pumvisible() ? "\<C-y><CR>" : "\<CR>"
+  set scrolloff=5
+]]
+
+vim.api.nvim_create_autocmd("UIEnter", {
+  callback = function ()
+    local groups = {
+      "Normal",
+      "NormalNC",
+      "NormalFloat",
+      "Float",
+      "FloatBorder",
+      "SignColumn",
+      "GitSignsAdd",
+      "GitSignsChange",
+      "GitSignsDelete",
+      -- "Pmenu",
+      -- "PmenuSel",
+      "WinSeparator",
+      "TelescopeNormal",
+      "TelescopeBorder",
+      "TelescopeSelection",
+      "TelescopePreviewNormal",
+      "WhichKeyFloat",
+    }
+
+    for _, group in ipairs(groups) do
+      -- vim.cmd("hi " .. group .. " ctermbg=None guibg=None")
+      vim.cmd("hi " .. group .. " guibg=None")
+    end
+
+    return true
+  end,
+  desc = "Transparent backgrounds",
+})
+
+
+-- =======================
+-- Misc vim autocmds
+-- =======================
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function()
+    -- local isNoNeckPainOn =
+    vim.cmd("lua require('no-neck-pain').enable()")
+    vim.cmd("lua require('quicknote').ToggleNoteSigns()")
+  end,
+})
