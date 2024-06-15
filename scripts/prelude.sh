bf3a9956d7e4e559f5ce700fce7d5f373aef0c59
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-15 18:33:58 +0800

    feat(scripts): fix prelude

--- scripts/prelude.sh
+++ scripts/prelude.sh
@@ -6,10 +6,13 @@
 command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; \
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
 
-
 if [ -f /opt/homebrew/bin/brew ]; then
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/kenyap/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if [ -f ~/.zprofile ]; then
+  source ~/.zprofile
 fi
 
 brew analytics off
