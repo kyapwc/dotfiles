b8f6a0a40667f5c8b77862f022de2d6ceeb7935f
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-20 22:23:16 +0800

    feat(hypr): add utility methods and some useful keybinds for shell

---
+++ hyprland/.config/hypr/scripts/brightness.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function get_brightness {
+  brightnessctl info | grep -oP '\(\K[0-9]+%'
+}
+
+function get_brightness_icon {
+  brightness_icon=""
+}
+
+function show_brightness_notif {
+  brightness=$(get_brightness)
+  get_brightness_icon
+  dunstify -t 1000 -r 2593 -u normal "$brightness_icon $brightness" -h int:value:$brightness -h string:hlcolor:$bar_color
+}
+
+case $1 in
+  up)
+    brightnessctl -s set +20%
+    show_brightness_notif
+  ;;
+  down)
+    brightnessctl -s set 20%-
+    show_brightness_notif
+  ;;
+esac
