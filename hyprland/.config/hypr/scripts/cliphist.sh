adba72f7feb43d59a5b3aa107b1ad38fc0fa25bd
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-21 01:13:01 +0800

    feat(hypr): add proper preview for cliphist images and normal text

---
+++ hyprland/.config/hypr/scripts/cliphist.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -modi clipboard:~/dotfiles/hyprland/.config/hypr/scripts/cliphist-rofi-img.sh -show clipboard -show-icons -theme-str '#window {height: 80%; }' -theme-str '#element-icon {size: 10ch; }'
