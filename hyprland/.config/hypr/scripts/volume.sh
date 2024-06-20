52a901a813239ec253cfb87572d9b5229582d087
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-06-20 23:14:05 +0800

    feat(hypr): add volume script

---
+++ hyprland/.config/hypr/scripts/volume.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+volume_step=5
+max_volume=100
+
+function get_volume {
+  pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1
+}
+
+function get_mute {
+  pactl get-sink-mute @DEFAULT_SINK@ | grep -Po '(?<=Mute: )(yes|no)'
+}
+
+function get_volume_icon {
+  volume=$(get_volume)
+  mute=$(get_mute)
+  if [ "$volume" -eq 0 ] || [ "$mute" == "yes" ] ; then
+    volume_icon=""
+  elif [ "$volume" -lt 50 ]; then
+    volume_icon=""
+  else
+    volume_icon=""
+  fi
+}
+
+function show_volume_notif {
+  volume=$(get_mute)
+  get_volume_icon
+  dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
+}
+
+case $1 in
+  up)
+    pactl set-sink-mute @DEFAULT_SINK@ 0
+    volume=$(get_volume)
+    if [ $(( "$volume" + "$volume_step" )) -gt $max_volume ]; then
+        pactl set-sink-volume @DEFAULT_SINK@ $max_volume%
+    else
+        pactl set-sink-volume @DEFAULT_SINK@ +$volume_step%
+    fi
+    show_volume_notif
+  ;;
+  down)
+    pactl set-sink-volume @DEFAULT_SINK@ -$volume_step%
+    show_volume_notif
+  ;;
+esac
