3f58f45da6efc3218d5b017c4c69083b9499c3c5
Yap Wei Chun <kyapwc@gmail.com>
2024-09-18 09:01:48 +0800

    feat: add new configs for neotest

--- nvim/.config/nvim/lua/yap/neotest.lua
+++ nvim/.config/nvim/lua/yap/neotest.lua
@@ -9,5 +9,27 @@ neotest.setup({
       end,
       discovery = { enabled = false },
     })
-  }
+  },
+  quickfix = {
+    enabled = true,
+    open = true,
+  },
+  running = { concurrent = true },
+})
+
+local function run_and_show_output()
+  neotest.run.run()
+end
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { "*.test.ts", "*.test.js" },
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>ns', ':lua require("neotest").summary.toggle()<CR>',
+      { desc = "Tests summary" })
+    vim.keymap.set('n', '<leader>nr', run_and_show_output, { buffer = 0, desc = "Run tests" })
+    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>np', ':lua require("neotest").output_panel.toggle()<CR>',
+      { desc = "Open output panel" })
+    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>no', ':lua require("neotest").output.open({ enter = true })<CR>',
+      { desc = "Show output" })
+  end,
 })
