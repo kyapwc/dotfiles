a10f6b652c178ce51f158cc88bd64c7201588057
Yap Wei Chun <kyapwc@gmail.com>
2025-01-15 15:12:21 +0800

    feat: install norg

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -657,4 +657,33 @@ return {
       })
     end,
   },
+
+  {
+    "nvim-neorg/neorg",
+    lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+    dependencies = {
+      "nvim-neorg/lua-utils.nvim",
+      "pysan3/pathlib.nvim",
+    }
+  },
 }
