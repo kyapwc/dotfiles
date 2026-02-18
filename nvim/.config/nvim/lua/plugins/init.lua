610a515a60a01560f86d8cea5fab64ca32ff05c9
Yap Wei Chun <kyapwc@gmail.com>
2026-02-18 15:01:58 +0800

    feat: remove rose-pine theme and install 99 to test

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -586,6 +586,93 @@ local plugins = {
       })
     end,
   },
+
+  {
+    "ThePrimeagen/99",
+    config = function()
+      local _99 = require("99")
+
+      -- For logging that is to a file if you wish to trace through requests
+      -- for reporting bugs, i would not rely on this, but instead the provided
+      -- logging mechanisms within 99.
+      local cwd = vim.uv.cwd()
+      local basename = vim.fs.basename(cwd)
+      _99.setup({
+        model = 'opencode/big-pickle',
+        logger = {
+          level = _99.DEBUG,
+          path = '/tmp/99.debug',
+          print_on_error = true,
+        },
+
+        --- Completions: #rules and @files in the prompt buffer
+        completion = {
+          -- I am going to disable these until i understand the
+          -- problem better.  Inside of cursor rules there is also
+          -- application rules, which means i need to apply these
+          -- differently
+          -- cursor_rules = "<custom path to cursor rules>"
+
+          --- A list of folders where you have your own SKILL.md
+          --- Expected format:
+          --- /path/to/dir/<skill_name>/SKILL.md
+          ---
+          --- Example:
+          --- Input Path:
+          --- "scratch/custom_rules/"
+          ---
+          --- Output Rules:
+          --- {path = "scratch/custom_rules/vim/SKILL.md", name = "vim"},
+          --- ... the other rules in that dir ...
+          ---
+          custom_rules = {
+            "scratch/custom_rules/",
+          },
+
+          --- Configure @file completion (all fields optional, sensible defaults)
+          files = {
+            -- enabled = true,
+            -- max_file_size = 102400,     -- bytes, skip files larger than this
+            -- max_files = 5000,            -- cap on total discovered files
+            -- exclude = { ".env", ".env.*", "node_modules", ".git", ... },
+          },
+
+          --- What autocomplete do you use.  We currently only
+          --- support cmp right now
+          source = "cmp",
+        },
+
+        --- WARNING: if you change cwd then this is likely broken
+        --- ill likely fix this in a later change
+        ---
+        --- md_files is a list of files to look for and auto add based on the location
+        --- of the originating request.  That means if you are at /foo/bar/baz.lua
+        --- the system will automagically look for:
+        --- /foo/bar/AGENT.md
+        --- /foo/AGENT.md
+        --- assuming that /foo is project root (based on cwd)
+        md_files = {
+          "AGENT.md",
+        },
+      })
+
+      -- take extra note that i have visual selection only in v mode
+      -- technically whatever your last visual selection is, will be used
+      -- so i have this set to visual mode so i dont screw up and use an
+      -- old visual selection
+      --
+      -- likely ill add a mode check and assert on required visual mode
+      -- so just prepare for it now
+      vim.keymap.set("v", "99", function()
+        _99.visual()
+      end)
+
+      --- if you have a request you dont want to make any changes, just cancel it
+      vim.keymap.set("v", "9s", function()
+        _99.stop_all_requests()
+      end)
+    end,
+  },
 }
 
 if not IS_DEVCONTAINER() then
